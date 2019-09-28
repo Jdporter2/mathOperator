fa8ea0c38de3ed9c18d763376c2b86fd7c035764
Jdporter2 <55327114+Jdporter2@users.noreply.github.com>
2019-09-28 12:22:55 -0500

    Initial commit

---
+++ mathPractice.py
@@ -0,0 +1,91 @@
+'''
+Created on Sep 28, 2019
+
+@author: ITAUser
+'''
+
+'''
+Multiplication
+'''
+a = 4
+b = 12
+
+c = 4 * 12
+print("c equals " + str(c))
+
+d = 6
+e = 2
+
+f = d * e
+print("f equals " + str(f))
+
+'''
+Addition
+'''
+a = 3
+b = 72
+
+c = a + b
+print(c)
+
+print(5+11)
+
+'''
+Division
+'''
+a = 12
+b = 3
+
+c = a / b
+print(c)
+
+a = 39
+b = 7
+
+c = 39 / 7
+print(c)
+
+'''
+Subtraction
+'''
+a = 8
+b = 2
+
+c = a - b
+print(c)
+
+a = 78
+b = 32
+
+c = a - b
+print(c)
+
+'''
+Floor Division
+'''
+a = 99
+b = 4
+
+c = a // b
+print(c)
+
+a = 47
+b = 3
+
+c = a // b
+print(c)
+
+'''
+Modulus
+'''
+a = 67
+b = 3
+
+c = a % b
+print(c)
+
+a = 17
+b = 3
+
+c = a % b
+print(c)

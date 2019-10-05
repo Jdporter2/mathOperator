3613dfb53b27d6562eda27300a3a7d0ea4a44f15
Jdporter2 <55327114+Jdporter2@users.noreply.github.com>
2019-10-05 10:42:55 -0500

    Create mathOperatorsDebug.py

---
+++ mathOperatorsDebug.py
@@ -0,0 +1,33 @@
+'''
+Created on Sep 28, 2019
+
+@author: ITAUser
+'''
+
+''' Incorrect: 40 * 0 = -1 --> -1 is not the correct answer, you could get rid of the = -1, then print the 40 * 0 '''
+
+print(40 * 0)
+
+''' Incorrect: “Hello 1 2 and” + 3 --> There needs to be a str(3) , instead of just 3. And a space after the and. '''
+
+print("Hello 1 2 and " + str(3))
+
+''' Incorrect: 6 _5 _1= 31 --> It should be 6 * 5 + 1 '''
+
+print(6*5+1)
+
+''' Incorrect: print(10 + "/" + 2) = ? --> It should be 10 / 2 '''
+
+print(10/2)
+
+''' print("LOL" * 3) ? --> Correct '''
+
+print("LOL" * 3)
+
+''' Incorrect: Print( "1" + "2" + "3") --> It should be print(1 + 2 + 3) '''
+
+print(1 + 2 + 3)
+
+''' Incorrect: print(10_2==0) --> It should be print(10 % 2==0) '''
+
+print(10 % 2 == 0)
